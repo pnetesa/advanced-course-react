659305525c1bc23408fb5fba034bdc53193886cf
Pavlo Netesa <pnetesa@gmail.com>
2023-10-10 16:40:34 +0300

    07 - Themes switching using ThemeProvider and useTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Link, Route, Routes } from 'react-router-dom';
-import './index.scss';
+import './styles/index.scss';
 import { MainPageLazy } from './pages/main-page/MainPage.lazy';
 import { AboutPageLazy } from './pages/about-page/AboutPage.lazy';
 import { Suspense } from 'react';
+import { useTheme } from './theme/use-theme';
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme} type="button">Toggle</button>
       <Link to={'/'}>Main</Link>
       <Link to={'/about'}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
