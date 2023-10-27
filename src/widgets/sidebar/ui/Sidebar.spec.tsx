f75625218a0d8580e0d8333171c3c9a3bde35b55
Pavlo Netesa <pnetesa@gmail.com>
2023-10-27 16:24:58 +0300

    Fixed unit and screenshot tests

--- src/widgets/sidebar/ui/Sidebar.spec.tsx
+++ src/widgets/sidebar/ui/Sidebar.spec.tsx
@@ -1,17 +1,17 @@
 /* eslint-disable i18next/no-literal-string */
 import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from 'widgets/sidebar';
-import { renderWithTranslation } from 'shared/lib/render-with-translation';
+import { renderComponent } from 'shared/lib/tests/render-component';
 
 describe('Sidebar component', () => {
   it('should render Sidebar', () => {
-    renderWithTranslation(<Sidebar />);
+    renderComponent(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     // screen.debug();
   });
 
   it('should toggle', () => {
-    renderWithTranslation(<Sidebar />);
+    renderComponent(<Sidebar />);
     const toggleBtn = screen.getByTestId('sidebar-toggle');
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     expect(screen.getByTestId('sidebar')).not.toHaveClass('collapsed');
