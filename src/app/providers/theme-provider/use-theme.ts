40406ca2e059d15b46f5ba222228db87404488c5
Pavlo Netesa <pnetesa@gmail.com>
2023-11-06 20:39:51 +0200

    31 - More fixes for styles

--- src/app/providers/theme-provider/use-theme.ts
+++ src/app/providers/theme-provider/use-theme.ts
@@ -10,9 +10,21 @@ export default function useTheme(): UseThemeResult {
   const { theme, setTheme } = useContext(ThemeContext);
 
   const toggleTheme = (): void => {
-    const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+    let newTheme: Theme;
+
+    switch (theme) {
+      case Theme.DARK:
+        newTheme = Theme.LIGHT;
+        break;
+      case Theme.LIGHT:
+        newTheme = Theme.DARK;
+        break;
+      default:
+        newTheme = Theme.LIGHT;
+    }
+
+    setTheme?.(newTheme);
     localStorage.setItem(LS_THEME_KEY, newTheme);
-    setTheme(newTheme);
   };
 
   return { theme, toggleTheme };
--- src/widgets/page-error/ui/PageError.tsx
+++ src/widgets/page-error/ui/PageError.tsx
@@ -3,6 +3,8 @@ import { classNames } from 'shared/lib/class-names';
 import cls from './PageError.module.scss';
 import { useTranslation } from 'react-i18next';
 import { Button } from 'shared/ui/button/Button';
+import { Theme } from 'app/providers/theme-provider';
+import { LS_THEME_KEY } from 'app/providers/theme-provider/ThemeContext';
 
 interface PageErrorProps {
   className?: string;
@@ -10,9 +12,10 @@ interface PageErrorProps {
 
 export const PageError: FC<PageErrorProps> = ({ className }): JSX.Element => {
   const { t } = useTranslation();
+  const theme = localStorage.getItem(LS_THEME_KEY) as Theme || Theme.LIGHT;
 
   return (
-    <div className={classNames(cls.pageError, [className])}>
+    <div className={classNames(cls.pageError, [className, theme])}>
       <p>{t('Unexpected error has occurred')}</p>
       <Button onClick={() => { location.reload(); }}>{t('Reload page')}</Button>
     </div>
